89fcaa25bf48afb1f47bfbe0f9c9c5bfd332712c
Sean Lin <seanl@intersect.org.au>
2013-04-21 05:11:40 +1000

    DC2B-95: Load manually collected data files
    Implement ImportCSV

--- scripts/ImportCSV.groovy
+++ scripts/ImportCSV.groovy
@@ -1,6 +1,103 @@
-def csv = new File(args[0])
+// Extract Sonde Data from CSV file
+// args[0]: CSV file path
+// args[1]: Database name(generated from grails environment)
+import static Constants.*
+import groovy.sql.Sql
+this.class.classLoader.rootLoader.addURL(new URL("file:///usr/share/java/postgresql-jdbc-8.4.701.jar"))
 
-csv.eachLine { line -> 
-    items = line.split(',')
-    println "INSERT INTO xxx VALUES('${items[0]}', '${items[1]}')"
+class Constants { 
+    // Sonde Survey Data Schema
+    // Date,Time,Latitude,Longitude,Water temperature,Sp Conductivity,Salinity,Depth,Turbidity,Battery,pH,Chlorophyll,Chlorophyll RFU,ODO Percent,ODO Concentration,BP
+    static final int              DATE = 0
+    static final int              TIME = 1
+    static final int          LATITUDE = 2
+    static final int         LONGITUDE = 3
+    static final int WATER_TEMPERATURE = 4
+    static final int   SP_CONDUCTIVITY = 5
+    static final int          SALINITY = 6
+    static final int             DEPTH = 7
+    static final int         TURBIDITY = 8
+    static final int           BATTERY = 9
+    static final int                PH = 10
+    static final int       CHLOROPHYLL = 11
+    static final int   CHLOROPHYLL_RFU = 12
+    static final int       ODO_PERCENT = 13
+    static final int ODO_CONCENTRATION = 14
+    static final int                BP = 15
+    }
+
+db = [url:"jdbc:postgresql://localhost:5432/${args[1]}", user:'aodn', password:'aodn', driver:'org.postgresql.Driver']
+sql = Sql.newInstance(db.url, db.user, db.password, db.driver)
+
+def csvfile = new File(args[0])
+csvfile.eachLine { line, index ->
+
+    if (index == 1) { // header line
+        // Validate schema
+        if (validateSchema(line.split(',')) == false) {
+            System.exit(-1)
+        }
+    }
+
+    if (index > 2) { // Ignore units line, start getting values
+        String[] values = line.split(',')
+        String foiId = getFoi(values)
+
+        if (!findFoi(foiId)) {
+            addFoi(foiId, values)
+        }
+
+        addToFoi(foiId, values)
+    }
+
+}
+
+private Boolean validateSchema(String[] attrs) {
+    // Date,Time,Latitude,Longitude,Water temperature,Sp Conductivity,Salinity,Depth,Turbidity,Battery,pH,Chlorophyll,Chlorophyll RFU,ODO Percent,ODO Concentration,BP
+    assertion = true
+    assertion = assertion && (attrs[DATE].equalsIgnoreCase('Date')) \
+    && (attrs[TIME].equalsIgnoreCase('Time')) \
+    && (attrs[LATITUDE].equalsIgnoreCase('Latitude')) \
+    && (attrs[LONGITUDE].equalsIgnoreCase('Longitude')) \
+    && (attrs[WATER_TEMPERATURE].equalsIgnoreCase('Water temperature')) \
+    && (attrs[SP_CONDUCTIVITY].equalsIgnoreCase('Sp Conductivity')) \
+    && (attrs[SALINITY].equalsIgnoreCase('Salinity')) \
+    && (attrs[DEPTH].equalsIgnoreCase('Depth')) \
+    && (attrs[TURBIDITY].equalsIgnoreCase('Turbidity')) \
+    && (attrs[BATTERY].equalsIgnoreCase('Battery')) \
+    && (attrs[PH].equalsIgnoreCase('pH')) \
+    && (attrs[CHLOROPHYLL].equalsIgnoreCase('Chlorophyll')) \
+    && (attrs[CHLOROPHYLL_RFU].equalsIgnoreCase('Chlorophyll RFU')) \
+    && (attrs[ODO_PERCENT].equalsIgnoreCase('ODO Percent')) \
+    && (attrs[ODO_CONCENTRATION].equalsIgnoreCase('ODO Concentration')) \
+    && (attrs[BP].equalsIgnoreCase('BP'))
+}
+
+private String getFoi(String[] attrs) {
+    sqlString = "SELECT GeometryFromText('POINT(" + (attrs[LONGITUDE] ?: 0) + " " + (attrs[LATITUDE] ?: 0) + ")', 4326) AS foi_id"
+
+    def foiId
+
+    sql.eachRow(sqlString) { row ->
+        foiId = row.foi_id
+    }
+
+    return foiId
+}
+
+private Boolean findFoi(String foiId) {
+    sqlString = "SELECT * FROM feature_of_interest WHERE feature_of_interest_id = ${foiId}"
+    rows = sql.rows(sqlString)
+
+    return rows.size() > 0
+}
+
+private void addFoi(String foiId, String[] attrs) {
+sql.execute("INSERT INTO feature_of_interest (feature_of_interest_id, feature_of_interest_name, feature_of_interest_description, geom, feature_type, schema_link) VALUES ('" + foiId + "', 'SHED', 'Sydney Harbour Environment Data', GeometryFromText('POINT(" + (attrs[LONGITUDE] ?: 0) + " " + (attrs[LATITUDE] ?: 0) + ")', 4326), 'sa:SamplingPoint', 'http://xyz.org/reference-url2.html')")
+    sql.execute('INSERT INTO foi_off VALUES (?, ?)', [foiId, 'GAUGE_HEIGHT'])
+    sql.execute('INSERT INTO proc_foi VALUES (?, ?)', ['urn:ogc:object:feature:Sensor:IFGI:ifgi-sensor-1', foiId])
+}
+
+private void addToFoi(String foi_id, String[] attrs) {
+    println("Going to add values")
 }
