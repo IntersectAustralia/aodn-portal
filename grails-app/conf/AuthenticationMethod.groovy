ca6432c83e9d63755a28d9894353afe56038102c
Sean Xiong <seanx@intersect.org.au>
2013-06-11 14:48:39 +1000

    Specify the Shibboleth sso url for production.

--- grails-app/conf/AuthenticationMethod.groovy
+++ grails-app/conf/AuthenticationMethod.groovy
@@ -7,7 +7,7 @@ authentication {
 	openIdProvider.url = "https://openid.emii.org.au/login"
 
 	aaf = "AAF"
-	shibboleth.url = "https://qa.dc2b.intersect.org.au/Shibboleth.sso"
+//	shibboleth.url = "https://qa.dc2b.intersect.org.au/Shibboleth.sso"
 }
 
 environments {
@@ -23,15 +23,18 @@ environments {
 	production {
 		// can be either "OpenID" Or "AAF".
 		authentication.method = "AAF"
+		shibboleth.url = "http://sho.sydney.edu.au/Shibboleth.sso"
 	}
 
 	staging {
 		// can be either "OpenID" Or "AAF".
 		authentication.method = "AAF"
+		shibboleth.url = "https://qa.dc2b.intersect.org.au/Shibboleth.sso"
 	}
 
 	qa {
 		// can be either "OpenID" Or "AAF".
 		authentication.method = "AAF"
+		shibboleth.url = "https://qa.dc2b.intersect.org.au/Shibboleth.sso"
 	}
 }
